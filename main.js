2a86f6ccd6b49017208e9af3d4b5ffebf8068581
Realm <realm@pop-os.localdomain>
2021-11-15 07:04:15 +0100

    refactor threejs code

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 import * as THREE from 'three';
-//scene, camera, renderer
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene();
+let scene, camera, renderer
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({
-  color: 'green'
+// Create the scene
+scene = new THREE.Scene();
+
+// const test = new THREE.TextureLoader();
+// const bgTexture = test.load('assets/bg.jpg');
+// scene.background = bgTexture;
+
+// Create the camera
+camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+camera.position.set(0,50,30)
+
+
+// Create the render
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create camera control for model
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update()
+
+// Add some lightning
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x898888 );
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight( 0x9b9898 );
+scene.add(dirLight);
+
+// Add a model to the scene
+const loader = new GLTFLoader();
+loader.load('assets/nature/scene.gltf', function(gltf) {
+  gltf.scene.scale.multiplyScalar(1 / 100); // adjust scalar factor to match your scene scale
+  gltf.scene.position.x = -100; // once rescaled, position the model where needed
+  gltf.scene.position.z = 2;
+  scene.add(gltf.scene)
 })
-const boxMesh = new THREE.Mesh(geometry, material)
-scene.add(boxMesh)
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
 }
 
-const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
-camera.position.set(0,1,2)
-scene.add(camera)
-
-const renderer = new THREE.WebGL1Renderer({
-  canvas: canvas
-});
-
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.shadowMap.enabled = true
-renderer.gammaOutput = true
-renderer.render(scene, camera)
+animate()
